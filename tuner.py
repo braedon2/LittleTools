dbf838258b0ac6e6e9cb866b90f31d4be62d7a79
Braedon McDonald <braedon.mcdonald@broadsign.com>
2023-06-03 16:58:39 -0400

    add curses to tuner

--- tuner.py
+++ tuner.py
@@ -7,11 +7,17 @@
 # License: Creative Commons Attribution-ShareAlike 3.0
 #          https://creativecommons.org/licenses/by-sa/3.0/us/
 ######################################################################
-import os
-import time
+
+######################################################################
+# Modified by:  Braedon McDonald
+# Date:    2023
+# License: Creative Commons Attribution-ShareAlike 3.0
+#          https://creativecommons.org/licenses/by-sa/3.0/us/
+######################################################################
 
 import numpy as np
 import pyaudio
+from curses import wrapper
 
 ######################################################################
 # Feel free to play with these numbers. Might want to change NOTE_MIN
@@ -45,7 +51,7 @@ NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
 def freq_to_number(f): return 69 + 12 * np.log2(f / 440.0)
 def number_to_freq(n): return 440 * 2.0 ** ((n - 69) / 12.0)
-def note_name(n): return NOTE_NAMES[n % 12] + str(n / 12 - 1)
+def note_name(n): return NOTE_NAMES[n % 12] + str(n // 12 - 1)
 
 
 ######################################################################
@@ -56,51 +62,51 @@ def note_name(n): return NOTE_NAMES[n % 12] + str(n / 12 - 1)
 def note_to_fftbin(n): return number_to_freq(n) / FREQ_STEP
 
 
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
+def main(stdscr):
+    imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
+    imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
 
-# Allocate space to run an FFT.
-buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-num_frames = 0
+    # Allocate space to run an FFT.
+    buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
+    num_frames = 0
 
-# Initialize audio
-stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                channels=1,
-                                rate=FSAMP,
-                                input=True,
-                                frames_per_buffer=FRAME_SIZE)
+    # Initialize audio
+    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                    channels=1,
+                                    rate=FSAMP,
+                                    input=True,
+                                    frames_per_buffer=FRAME_SIZE)
 
-stream.start_stream()
+    stream.start_stream()
 
-# Create Hanning window function
-window = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, SAMPLES_PER_FFT, False)))
+    # Create Hanning window function
+    window = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, SAMPLES_PER_FFT, False)))
 
-# Print initial text
-print('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
-print(" ")
+    while stream.is_active():
 
-# As long as we are getting data:
-while stream.is_active():
+        # Shift the buffer down and new data in
+        buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
+        buf[-FRAME_SIZE:] = np.frombuffer(stream.read(FRAME_SIZE), np.int16)
 
-    # Shift the buffer down and new data in
-    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-    buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
+        # Run the FFT on the windowed buffer
+        fft = np.fft.rfft(buf * window)
 
-    # Run the FFT on the windowed buffer
-    fft = np.fft.rfft(buf * window)
+        # Get frequency of maximum response in range
+        freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
 
-    # Get frequency of maximum response in range
-    freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
+        # Get note number and nearest note
+        n = freq_to_number(freq)
+        n0 = int(round(n))
 
-    # Get note number and nearest note
-    n = freq_to_number(freq)
-    n0 = int(round(n))
+        # Console output once we have a full buffer
+        num_frames += 1
 
-    # Console output once we have a full buffer
-    num_frames += 1
+        if num_frames >= FRAMES_PER_FFT:
+            s = 'freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(freq, note_name(n0), n - n0)
+            stdscr.clear()
+            stdscr.addstr(s)
+            stdscr.refresh()
 
-    if num_frames >= FRAMES_PER_FFT:
-        print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
-            freq, note_name(n0), n - n0))
-    os.system('clear')
-        #time.sleep(0.1)
+
+wrapper(main)
+
